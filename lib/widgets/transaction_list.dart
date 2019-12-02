43b8e66ec1bf661f1682725a0b3d420302004c81
Aaron <gitlab@webstish.io>
2019-12-02 18:03:01 -0500

    Replace SingleChildScrollView with ListView widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,55 +11,54 @@ class TransactionList extends StatelessWidget {
     Widget build(BuildContext context) {
         return Container(
             height: 300,
-            child: SingleChildScrollView(
-              child: Column(
-                  children: this.transactions.map((tx) {
-                      return Card(
-                          child: Row(
-                              children: <Widget>[
-                                  Container(
-                                      decoration: BoxDecoration(
-                                          border: Border.all(
-                                              color: Colors.purple,
-                                              width: 2,
-                                          ),
-                                      ),
-                                      padding: EdgeInsets.all(10),
-                                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                                      child: Text(
-                                          '\$${tx.amount}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20,
-                                              color: Colors.purple,
-                                          ),
-                                      ),
-                                  ),
-                                  Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                          Text(
-                                              tx.title,
-                                              style: TextStyle(
-                                                  fontSize: 17,
-                                                  fontWeight: FontWeight.normal,
-                                              )
-                                          ),
-                                          Text(
-                                              DateFormat.yMMMd().format(tx.date),
-                                              style: TextStyle(
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.w400,
-                                                  fontStyle: FontStyle.italic,
-                                                  color: Colors.grey,
-                                              )
-                                          ),
-                                      ],
-                                  )
-                              ],
-                          ));
-                  }).toList()
-              ),
+            child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                        child: Row(
+                            children: <Widget>[
+                                Container(
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.purple,
+                                            width: 2,
+                                        ),
+                                    ),
+                                    padding: EdgeInsets.all(10),
+                                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                                    child: Text(
+                                        '\$${transactions[index].amount.toStringAsFixed(2)}',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            color: Colors.purple,
+                                        ),
+                                    ),
+                                ),
+                                Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                        Text(
+                                            transactions[index].title,
+                                            style: TextStyle(
+                                                fontSize: 17,
+                                                fontWeight: FontWeight.normal,
+                                            )
+                                        ),
+                                        Text(
+                                            DateFormat.yMMMd().format(transactions[index].date),
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w400,
+                                                fontStyle: FontStyle.italic,
+                                                color: Colors.grey,
+                                            )
+                                        ),
+                                    ],
+                                )
+                            ],
+                        ));
+                },
+                itemCount: this.transactions.length,
             ),
         );
     }
